33b8f1fffd294970460b9318a815c868e88fe287
Diego Pineda <pineda.garcia.404@gmail.com>
2026-02-02 08:13:50 -0500

    Agregar tema minimal

--- nvim/lua/plugins/themes/minimal.lua
+++ nvim/lua/plugins/themes/minimal.lua
@@ -1,10 +1,9 @@
-return {
-  {
-    'lazy.nvim',
-    name = 'minimal-theme-loader', -- A name makes it easier to identify
-    enabled = false,
-    init = function()
-      -- vim.cmd.colorscheme 'minimal'
-    end,
-  },
-}
+-- Obsidian Minimal theme
+-- El colorscheme está en ~/.config/nvim/colors/obsidian-minimal.lua
+-- No es un plugin externo, es un tema local
+
+-- Para activar Obsidian Minimal, DESCOMENTA la siguiente línea:
+vim.cmd.colorscheme 'obsidian-minimal'
+
+-- Retornamos tabla vacía porque no hay plugin que instalar
+return {}
